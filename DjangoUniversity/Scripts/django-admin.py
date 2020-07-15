6a17dda3d8c561c7ea7ae8baf1ae8181b0abfeab
Alex Moffat <wamoffat4@gmail.com>
2020-07-15 15:12:31 -0700

    Create DjangoUniversity
    - Create a new project in virtual environment PyCharm named 'DjangoUniversity'
    - Install django for this environment
    - Start project 'Classes'
    - Create a superuser to log in as an admin.
    - Create a new app within project.
    - Create a class within models.py file with required attributes.
    - Command line create three objects new class and assign attribute values.
    - Add model to the admin.py file and register it.
    - Add the classApp name to the Main Project settings.py file.
    - Make a migration and migrate it for the three objects created.

--- DjangoUniversity/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\alexander\desktop\git_local\python-projects\djangouniversity\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

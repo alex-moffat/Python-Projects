041d2754263112b7adb86dfbc3ad957f61a8d1db
Alex Moffat <wamoffat4@gmail.com>
2020-09-28 12:44:13 -0700

    Remove Django University

--- DjangoUniversity/Classes/classApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-#========== IMPORT model (Class) from module 'models'
-from .models import DjangoClasses
-
-#========== REGISTER model (Class) inside 'products' app
-admin.site.register(DjangoClasses)
--- DjangoUniversity/Classes/classApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassappConfig(AppConfig):
-    name = 'classApp'
